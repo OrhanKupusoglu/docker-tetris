7dee9364f59c32c2f2b20a10b9ec3e165060a57d
Orhan Kupusoglu <orhan.kupusoglu@>
2018-06-15 17:05:43 +0300

    Add HTTP port to /etc/environment and activate virtual env first

--- docker/start.sh
+++ docker/start.sh
@@ -9,6 +9,9 @@ arg=$1
 DIR_RUN="$(cd "$(dirname "$0")" && pwd)"
 cd $DIR_RUN
 
+# activate virtual env first
+source .venv/bin/activate
+
 if [[ "$arg" == "mod" ]]
 then
     export TETRIS_SERVER_SETTINGS="${DIR_RUN}/settings.cfg"
@@ -16,7 +19,7 @@ fi
 
 export FLASK_APP=tetris-server/app.py
 
-source .venv/bin/activate; nohup flask run --host=0.0.0.0 --port=${FLASK_HTTP_PORT} > /dev/null 2>&1 &
+nohup flask run --host=0.0.0.0 --port="${FLASK_HTTP_PORT}" > /dev/null 2>&1 &
 
 echo "-- Flask HTTP port: ${FLASK_HTTP_PORT}"
 echo "-- started"
